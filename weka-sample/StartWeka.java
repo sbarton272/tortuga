daf1df91eda500d14577a85f7220dbca6cd3f222
Jackie Harris <jdharris@andrew.cmu.edu>
2014-06-21 10:01:35 -0400

    Added LibSVM as the classifier, added more notes.

--- weka-sample/StartWeka.java
+++ weka-sample/StartWeka.java
@@ -2,16 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Random;
 
+import weka.classifiers.functions.LibSVM;
 import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
+
 
 public class StartWeka {
 
 	public static void main(String[] args) throws Exception {
 
 		if(args.length < 1) {
-				System.out.println("Sorry, I need a file path provided.");
+			System.out.println("Sorry, I need a file path provided.");
+			return;
 		}
 
 		BufferedReader bufferedReader = null;
@@ -22,10 +24,10 @@ public class StartWeka {
 
 		bufferedReader.close();
 
-		NaiveBayes nB = new NaiveBayes();
-		nB.buildClassifier(train);
+		LibSVM lsvm = new LibSVM();
+		lsvm.buildClassifier(train);
 		Evaluation eval = new Evaluation(train);
-		eval.crossValidateModel(nB, train, 10, new Random(1));
+		eval.crossValidateModel(lsvm, train, 10, new Random(1));
 		System.out.println(eval.toSummaryString("\nResults\n======\n", true));
 		System.out.println(eval.fMeasure(1) + " " + eval.precision(1) + " " + eval.recall(1));
 	}
