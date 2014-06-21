8c662c02637f85f573842f2d1ca31d97a926fc20
436C69 <clearchengli@yahoo.com>
2014-06-21 01:11:38 -0700

    added the accelerometer code

--- main-project/src/org/turtles/tortuga/MainActivity.java
+++ main-project/src/org/turtles/tortuga/MainActivity.java
@@ -1,18 +1,24 @@
 package org.turtles.tortuga;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+	
+	private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +28,24 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float x = event.values[0];
+	    float y = event.values[1];
+	    float z = event.values[2];
+	    
+	    System.out.println(x + ", " + y + ", " + z);
+	}
+	 
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	 
 	}
 
 	@Override
@@ -60,5 +84,4 @@ public class MainActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
-
 }
