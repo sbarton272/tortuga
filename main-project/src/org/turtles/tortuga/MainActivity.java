2dac22dfe37d091aa58a3632ae584e2344a6d005
436C69 <clearchengli@yahoo.com>
2014-06-21 03:40:58 -0700

    Initial audio recording code... the audio is having a problem and it's not recorded yet

--- main-project/src/org/turtles/tortuga/MainActivity.java
+++ main-project/src/org/turtles/tortuga/MainActivity.java
@@ -1,30 +1,38 @@
 package org.turtles.tortuga;
 
+import java.util.Arrays;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.AudioRecord.OnRecordPositionUpdateListener;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.widget.Button;
-import android.webkit.WebView;
-import android.webkit.WebSettings;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity implements SensorEventListener {
 	
 	private SensorManager mSensorManager;
     private Sensor mAccelerometer;
+    private AudioRecord mAudioRecorder;
+    
+    private static final int RECORDER_SAMPLE_RATE = 8000;
+    private static final int AUDIO_BUFFER_SIZE = 4096;
+    private byte[] audioBuffer = new byte[AUDIO_BUFFER_SIZE];
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +44,18 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		
+		mAudioRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+	            RECORDER_SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+	            AudioFormat.ENCODING_PCM_16BIT, AUDIO_BUFFER_SIZE);
+		
+		mAudioRecorder.setRecordPositionUpdateListener(new PeriodicListener());
+		mAudioRecorder.setPositionNotificationPeriod(100);
+		mAudioRecorder.startRecording();
+		
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		
 	}
 	
 	@Override
@@ -47,9 +64,9 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 	    float y = event.values[1];
 	    float z = event.values[2];
 	    
-	    System.out.println(x + ", " + y + ", " + z);
+	    //System.out.println(x + ", " + y + ", " + z);
 	}
-	 
+	
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	 
@@ -130,6 +147,25 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 	         });
 			
 			return rootView;
+		}
+	}
+	
+	public class PeriodicListener implements OnRecordPositionUpdateListener {
+
+		@Override
+		public void onMarkerReached(AudioRecord arg0) {
+			// TODO Auto-generated method stub
+			
+			System.out.println("Hello");
+			
+		}
+
+		@Override
+		public void onPeriodicNotification(AudioRecord arg0) {
+			
+			System.out.println("Hello2");
+			mAudioRecorder.read(audioBuffer, 0, AUDIO_BUFFER_SIZE);
+			System.out.println(Arrays.toString(audioBuffer));
 		}
 	}
 }
